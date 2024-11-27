ed33969616ed96d054df335a22505c5f63cde5a0
Kredding0055 <redkr3815@gmail.com>
2024-11-26 21:32:41 -0800

    worked on the modals

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,6 +1,6 @@
 // frontend/src/components/LoginFormPage/LoginFormPage.jsx
 
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal'; 
@@ -8,11 +8,25 @@ import './LoginFormModal.css';
 
 
 function LoginFormModal() {
+  const { setModalContent, closeModal } = useModal();
   const dispatch = useDispatch();
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
+  const [loginDisabled, setLoginDisabled] = useState(true)
   const [errors, setErrors] = useState({});
-  const { closeModal} = useModal();
+
+  const validateLogin = useCallback(() => {
+    if(credential.length >= 4 && password.length >= 6) {
+      setLoginDisabled(false)
+    }
+    else {
+      setLoginDisabled(true)
+    }
+  }, [credential, password, setLoginDisabled]);
+
+  useEffect(() => {
+    validateLogin();
+  }, [validateLogin]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,30 +41,53 @@ function LoginFormModal() {
       });
   }
 
+  // const demoUserLogin = () => {
+    
+  // }
+
   return (
-    <>
+    <div className='login-modal-container'>
+      {errors && (
+        <div className="error-message">
+          {Object.keys(errors).map((key, index) => (
+          <p key={index}>{errors[key]}</p>
+          ))}
+        </div>
+      )}
       <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
           <input
+            className='input-fields'
             type="text"
             placeholder='Username or Email'
             value={credential}
-            onChange={(e) => setCredential(e.target.value)}
+            onChange={(e) => {setCredential(e.target.value); validateLogin()}}
             required
           />
+          <br/>
+          <br/>
           <input
+            className='input-fields'
             type="password"
             placeholder='Password'
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {setPassword(e.target.value); validateLogin()}}
             required
           />
         {errors.credential && (
           <p>{errors.credential}</p>
         )}
-        <button type="submit">Log In</button>
+        <br/>
+        <br/>
+        <button
+        className='login-button'
+        type="submit"
+        disabled={loginDisabled}
+        >Log In</button>
       </form>
-    </>
+        <br/>
+      <button className='demo-button'>Demo User</button>
+    </div>
   );
 }
 
